315960db0ab8df8f085506893519fcd36164e7f3
Sharath <sharathholmes143@gmail.com>
2022-09-20 15:15:54 +0530

    Implemented Compose New Mail Page and Reply and reply All buttons

--- src/main/java/com/sharath/mailbox/Controllers/EmailViewController.java
+++ src/main/java/com/sharath/mailbox/Controllers/EmailViewController.java
@@ -17,8 +17,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
